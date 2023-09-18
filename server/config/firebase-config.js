420a5d89bee54e9ddcb49b76339358473d7c1627
21st-Athena <132208831+21st-Athena@users.noreply.github.com>
2023-09-18 15:22:09 +0200

    firebase-config

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,11 +1,19 @@
-import { initializeApp, cert } from "firebase-admin/app";
+// import { initializeApp, cert } from "firebase-admin/app";
+import admin from 'firebase-admin';
+// const admin = require('firebase-admin')
 import { getAuth } from "firebase-admin/auth";
 
-import serviceAccountKey from "./serviceAccountKey.json";
+const serviceAccount = {
+  projectId: "react-chat-88b4f",
+  privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCRodapnpQWxfCk\nLaHPRElgw9k1Q9LC4Sxbf66EPnUmEHg9sh0cTjlBFpRwojt2qgObTmXQ9AldibL0\nGsIfhzVlf4O1ZG2pkLEGURC0ju1Gy5y5RdOutSHc78mTeo9c6bksrzPsqcy24gPS\njevjK6n0y/EUhj+nKczf0LXPxaZGEVx6bUcRgWKDtSPZhsWobF2wqXJVwxYiKNMq\nlsZCsYTjIkMefu3FcccVJ7lV7BtHLsO9zM3zoMONsP3jOgcN+eAc4cq5vnRFnTQ+\nJbk6aZC2V0l30n6qVh0hgOf2a1UFZivI15J0+W2OLKUoy9pQoZIqz8WpcL1EjHOm\nPBo3TORvAgMBAAECggEAQ/0OPnnuIQOf6MX0+NKkfyxvx1I2nwyIrN7bFpoiDvc5\na3Qv6vsJ970W586VfDy7GlJrbjW/+Y9DoHqtjhKNdykk0BZmq/cE7jICXokrdRWx\nwT08KiAeAwtfTB63t2TWMmpJD3/mNqslpbLkIVZNLh/YGQDjzqLRJtxFRQ69JZBY\n3FpHpyKfac1+5LOYxdxQvJoMXeLi73DvtbW5GP3u8+1thr/xAIqGyTTumyRThzWG\nig8IBx2DxC0VFULZsweAeUKmV7U7IDcMkfIAIPvHbVdvBUD99jjbDop1duNORhzp\n/i8Bgt5X1HK7y1diUbBgQdvJmiZk90hsJRfKhQL9IQKBgQDLDqn4Jbyzon55xvzX\n0nPRAiYcYGevL9bZGEFZQcEP3BE9dduFSdDfChJfCuEV7CQ5taZ0xIQbnrlT/JV+\nI5Uys9dTiHo02nRM/MzRmxqDq4YPI9fQGgu555UU+CHaQxDmJUidBo8Gds5mQff/\nepDvhk3jP3MKx4GFCtXkWMjMJQKBgQC3mkH8efTjyi39faZ/rdVJGexik5oddsGA\nMODdgHZMP1txl8qaC647hApo/DNLaK/+EgKFCw20eFCUKQts65aMq/wtisxXEZR/\nbYmeETPfA4fcrXlEG23Axor3bd1EMZbE05PH3XcQjVKp4gPVrE6rZIPuNlDXFsBn\nFKfphWCAAwKBgFd5Ea9cUlXwAhvjpLg0aL/49QH0ZbYKHbuXv078DHcQjjyzsMVS\nbzAl3pQi06Pl/hT+M57JqT9cuOl9PckA+xzEbFYHzN1H3iHsJS+GaX2fPjj3b9it\n369OG+LUoYCss3LxTL6YPLTEKVXTxUnBOv50FMl4Y86Y8yqk6E9eD+IhAoGAe74h\nvxJLNMNtH63x65ezamNoaD/m3PjPDaDjzSXplRYWgYa2bAB3W3FwurVPtQFHrEtj\nG1wJzppQZKsNIknd282GVwN3GS+mEAQ5filF9oagqODaHEcIP6cQvhZzzKRHYzze\nGxRuTN+S2SCVSQuqseU67e/5qB2Ta2YGjWLxWRUCgYBfh9trqnaj1922VwbjX297\nVzUTD1/GSKfzmshc+mSL8zSkX6WOp32m2ECPhkAccLRNwbafPClj5pThisEAhR3T\n/87dfeNEI5lj+wCrep9vAWXjiEUxs6K39Q4estIo+oSpkAzcSnvbutsjj1zH79oI\nz9SeYCaW9AoxsXMQvjMFkA==\n-----END PRIVATE KEY-----\n",
+  clientEmail: "firebase-adminsdk-zoye6@react-chat-88b4f.iam.gserviceaccount.com",
+}
 
-const app = initializeApp({
-  credential: cert(serviceAccountKey),
+const app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
 });
 
 const auth = getAuth(app);
 export default auth;
+
+
--- server/config/mongo.js
+++ server/config/mongo.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = 'mongodb://127.0.0.1:27017/chat';
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
