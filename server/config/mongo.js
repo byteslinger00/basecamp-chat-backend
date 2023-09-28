a29e37b0c4ac8be855bddfd475ec52378f481e4d
21st-Athena <132208831+21st-Athena@users.noreply.github.com>
2023-09-28 09:48:23 +0200

    change/mongo-uri

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
  
-// const MONGO_URI = 'mongodb://127.0.0.1:27017/chat';
+const MONGO_URI = 'mongodb://127.0.0.1:27017/chat';
 // const MONGO_URI = 'mongodb+srv://akhil-test:clJWYZ15jty0IcXY@cluster0.8vmew3g.mongodb.net/';
-const MONGO_URI = 'mongodb+srv://grupapoydev:black123qwe@cluster0.c9v307y.mongodb.net/';
+// const MONGO_URI = 'mongodb+srv://grupapoydev:black123qwe@cluster0.c9v307y.mongodb.net/';
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
